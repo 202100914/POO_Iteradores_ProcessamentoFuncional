b423b842a0645e6e4ef686e004a108baf975fa44
202100914 <202100914@estudantes.ips.pt>
2024-09-20 15:49:31 +0100

    Begin

---
+++ SandBox/src/main/java/StreamingService.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class StreamingService {
+    private List<Account> activeAccounts;
+    /**
+     * Constructor of class StreamingService.
+     */
+    public StreamingService() {
+
+    }
+    /**
+     * Get the list of accounts.
+     *
+     * @return This streaming service active accounts.
+     */
+    public List<Account> getActiveAccounts() {
+
+    }
+    /**
+     * Add an account.
+     *
+     * @param account to add.
+     */
+    public void addAccount(Account account) {
+
+    }
+    /**
+     * Remove an account.
+     *
+     * @param account to remove.
+     */
+    public void removeAccount(Account account) {
+
+    }
+    /**
+     * Remove all accounts that didn't pay. Implementation using an iterator.
+     */
+    public void removeUnpaidAccounts() {
+
+    }
+    /**
+     * Clear all payments. Implementation using functional
+     * programming.
+     */
+    public void setAllAccountsAsUnpaid() {
+
+    }
+    /**
+     * Remove all accounts that didn't pay. Implementation using removeIf method.
+     */
+    public void removeUnpaidAccountsRemoveIf() {
+
+    }
+    /**
+     * Get the list of accounts that didn't pay. Implementation using functional
+     * programming.
+     *
+     * @return A list with all accounts that didn't pay.
+     */
+    public List<Account> getListOfUnpaidAccounts() {
+
+    }
+    /**
+     * Count the amount of accounts that didn't pay. Implementation using
+     * functional programming.
+     *
+     * @return The amount of accounts that didn't pay.
+     */
+    public long countUnpaidAccounts() {
+
+    }
+    @Override
+    public String toString() {
+
+    }
+}
